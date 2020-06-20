062a783d457375f52fb05581216455c045da35f0
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-06-20 11:58:41 -0600

    Added IConstants with color info

---
+++ src/model/IConstants.java
@@ -0,0 +1,9 @@
+package model;
+
+public interface IConstants {
+    public static final int RED = -65536;
+    public static final int GREEN = -16746496;
+    public static final int LIGHT = -126;
+    public static final int BOX = -2960686;
+    public static final int BACKGROUND = -26;
+}
