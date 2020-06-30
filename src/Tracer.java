2daae3f9be47d28a016b1d1674ed21eaed8dc59c
Gabriel Vargas <gvargasr@estudiantec.cr>
2020-06-29 22:10:39 -0600

    Added intersection demo in main and added direction to main algorithm

--- src/Tracer.java
+++ src/Tracer.java
@@ -1,5 +1,5 @@
 import model.*;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 import java.util.ArrayList;
@@ -26,6 +26,8 @@ public class Tracer implements IConstants{
 
     public void tracePath(){
         model.Point point;
+        model.Point dirPoint;
+        model.Point direction;
         int[] pixColor;
         int[] calculatedColor;
 
@@ -34,7 +36,10 @@ public class Tracer implements IConstants{
             pixColor = new int[]{0,0,0}; // RGB value calculated for the point at a given moment inside the while loop
             
             for (int sample = 0; sample < SAMPLE_SIZE; sample++){
-                calculatedColor = calculatePixel();
+                // every sample is taken in random directions
+                dirPoint = new model.Point(random.nextInt(IMAGE_SIZE), random.nextInt(IMAGE_SIZE));
+                direction = Intersector.normalize(dirPoint.subtract(point)); // normalized for accuracy and simplifying calculations
+                calculatedColor = calculatePixel(poinr, direction, 0);
 
                 // adds the calculated RGB values to the current color for each sample taken
                 pixColor[0] += calculatedColor[0];
@@ -42,7 +47,7 @@ public class Tracer implements IConstants{
                 pixColor[2] += calculatedColor[2];
             }
 
-            // Averages the color values received using the amount of samples taken
+            // averages the color values received using the amount of samples taken
             pixColor[0] = (int) (pixColor[0] / SAMPLE_SIZE);
             pixColor[1] = (int) (pixColor[1] / SAMPLE_SIZE);
             pixColor[2] = (int) (pixColor[2] / SAMPLE_SIZE);
@@ -52,12 +57,38 @@ public class Tracer implements IConstants{
         }
     }
 
-    private int[] calculatePixel(){
+    private int[] calculatePixel(Point pOrigin, Point pDirection, int pDepthCount){
+
+        if (pDepthCount == TRACE_DEPTH){
+            // por mientras así, pero mejor poner que trace directo a algún source
+            return new int[]{0,0,0}; // return black
+        }
+
+        
         return null;
     }
 
     public static void main(String[] args) {
-        Main m = new Main();
-        m.trace();
+        // Main m = new Main();
+        // m.trace();
+
+        Random rand = new Random();
+
+        model.Point o = new Point(rand.nextInt(6), rand.nextInt(6));
+        model.Point d = new Point(rand.nextInt(6), rand.nextInt(6));
+        model.Point dir = d.subtract(o);
+        model.Point s1 = new Point(3,5);
+        model.Point s2 = new Point(3,1);
+        model.Point l1 = new Point(5,3);
+        model.Point l2 = new Point(5,5);
+
+        int i1 = Intersector.intersection(o, Intersector.normalize(dir), s1, s2);
+        int i2 = Intersector.intersection(o, Intersector.normalize(dir), l1, l2);
+
+        // Al ser un grid tan pequeño, pueden hacer errores de precisión por rounding en el Point division
+        System.out.println("Point: " + o + " d: "+ d);
+        System.out.println("Direction: " + dir + ", Normalized: " + Intersector.normalize(dir) );
+        if (i1 != -1) System.out.println("inter1: "+Intersector.intersectionPoint(o, Intersector.normalize(dir), i1));
+        if (i2 != -1) System.out.println("inter2: "+Intersector.intersectionPoint(o, Intersector.normalize(dir), i2));
     }
 }
